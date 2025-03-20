0de0f20547cfb909c4f8e479f23ddb1bb13f1c4f
Cyrille Rossant <rossant@users.noreply.github.com>
2025-03-20 21:13:41 +0100

    Update build.sh for slimshady

--- signals/build.sh
+++ signals/build.sh
@@ -2,4 +2,6 @@ DATOVIZ_FOLDER="../../datoviz"
 gcc -I$DATOVIZ_FOLDER/include \
     -I$DATOVIZ_FOLDER/build/_deps/cglm-src/include/ \
     -L$DATOVIZ_FOLDER/build \
-    slimshady.c -o slimshady -ldatoviz
+    slimshady.c -o slimshady \
+    -ldatoviz \
+    -Wl,-rpath,$DATOVIZ_FOLDER/build
