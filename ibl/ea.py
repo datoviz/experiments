b83cd1170ef64a27c82d59e46794b5e9add3cb85
Cyrille Rossant <rossant@users.noreply.github.com>
2025-04-18 14:50:11 +0200

    Improve ephys atlas demo

--- ibl/ea.py
+++ ibl/ea.py
@@ -9,7 +9,7 @@ import urllib.request
 from iblatlas import atlas
 from pywavefront import Wavefront
 import datoviz as dvz
-from datoviz import vec2
+from datoviz import Out, vec2, cvec4
 
 
 CCF_URL = 'http://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf/annotation/ccf_2017/structure_meshes/'
@@ -19,6 +19,11 @@ br = BrainRegions()
 mapping = 'Allen'
 label = 'latest'
 hemisphere = 'left'
+point_alpha = 255
+cmap = dvz.CMAP_MAGMA
+quantile = .01
+point_size = 3
+font_size = 64
 
 
 def lateralize_features(df):
@@ -103,7 +108,7 @@ def add_mesh(batch, panel, pos, idx, color, alpha=255):
     return visual
 
 
-def add_points(batch, panel, pos, color, size):
+def add_points(batch, panel, pos, size):
     n = pos.shape[0]
     pos = np.ascontiguousarray(pos, dtype=np.float32)
 
@@ -112,19 +117,32 @@ def add_points(batch, panel, pos, color, size):
 
     dvz.point_alloc(visual, n)
     dvz.point_position(visual, 0, n, pos.astype(np.float32), 0)
-    dvz.point_color(visual, 0, n, color.astype(np.uint8), 0)
     dvz.point_size(visual, 0, n, size.astype(np.float32), 0)
     dvz.panel_visual(panel, visual, 0)
 
     return visual
 
 
+def set_feature(idx):
+    feature = features[idx]
+    values = df_voltage[feature]
+    a, b = np.quantile(values, quantile), np.quantile(values, 1-quantile)
+    channel_color = dvz.cmap(cmap, values, a, b)
+    channel_color[:, 3] = point_alpha
+    dvz.point_color(points, 0, n, channel_color.astype(np.uint8), 0)
+
+
+def set_label(text):
+    dvz.glyph_strings(
+        glyph, 1, [text],
+        np.array([[0, 1, 0]], dtype=np.float32), np.array([1], np.float32),
+        cvec4(16, 16, 16, 255), vec2(0, -50), vec2(.5, .5),
+    )
+
+
 # Load EA data.
 df_voltage, df_channels, df_channels, df_probes = \
     load_voltage_features(local_data_path.joinpath(label), mapping=mapping)
-df_voltage['atlas_id'] = df_channels['atlas_id'].astype(np.int32)
-df_voltage['atlas_idx'] = np.array(
-    [_[0] for _ in br.id2index(df_voltage.atlas_id)[1]], dtype=np.int32)
 df_voltage = lateralize_features(df_voltage)
 df_voltage.drop(
     df_voltage[df_voltage[mapping + "_acronym"].isin(["void", "root"])].index, inplace=True)
@@ -132,13 +150,8 @@ channel_pos = np.ascontiguousarray(df_voltage[['x', 'y', 'z']], dtype=np.float32
 n = channel_pos.shape[0]
 print(f"Loaded {n} channels")
 
-values = df_voltage['psd_alpha']
-channel_color = dvz.cmap(dvz.CMAP_HSV, values, values.min(), values.max())
-
-# values = df_voltage['psd_alpha']
-# m, M = values.min(), values.max()
-# channel_size = 2 + 8 * (values.values - m) / (M - m)
-channel_size = np.full(n, 3, dtype=np.float32)
+features = sorted(voltage_features_set())
+channel_size = np.full(n, point_size, dtype=np.float32)
 
 
 # Load mesh.
@@ -159,12 +172,30 @@ channel_pos -= center
 channel_pos *= 200
 mesh_pos *= 200
 
+selected = Out(0)
+
 
 @dvz.gui
 def ongui(app, fid, ev):
-    dvz.gui_size(vec2(170, 110))
+    dvz.gui_size(vec2(300, 100))
     dvz.gui_begin("GUI", 0)
+    if dvz.gui_dropdown("Feature", len(features), features, selected, 0):
+        set_feature(selected.value)
+        set_label(features[selected.value])
     dvz.gui_end()
+
+
+@dvz.keyboard
+def on_keyboard(app, window_id, ev):
+    if ev.type in (dvz.KEYBOARD_EVENT_PRESS, dvz.KEYBOARD_EVENT_REPEAT):
+        if ev.key == dvz.KEY_LEFT:
+            selected.value = len(features) - 1 if selected.value == 0 else selected.value - 1
+            set_feature(selected.value)
+            set_label(features[selected.value])
+        elif ev.key == dvz.KEY_RIGHT:
+            selected.value = 0 if selected.value == len(features) - 1 else selected.value + 1
+            set_feature(selected.value)
+            set_label(features[selected.value])
 
 
 # Application.
@@ -175,10 +206,20 @@ figure = dvz.figure(scene, 1920, 1080, dvz.CANVAS_FLAGS_IMGUI)
 panel = dvz.panel_default(figure)
 arcball = dvz.panel_arcball(panel)
 
-add_points(batch, panel, channel_pos, channel_color, channel_size)
-add_mesh(batch, panel, mesh_pos, mesh_idx, mesh_color, alpha=32)
+points = add_points(batch, panel, channel_pos, channel_size)
+mesh = add_mesh(batch, panel, mesh_pos, mesh_idx, mesh_color, alpha=32)
+set_feature(0)
+
+glyph = dvz.glyph(batch, 0)
+dvz.visual_fixed(glyph, True, True, True)
+af = dvz.atlas_font(font_size)
+dvz.glyph_atlas_font(glyph, af)
+set_label(features[0])
+dvz.panel_visual(panel, glyph, 0)
+
 
 dvz.app_gui(app, dvz.figure_id(figure), ongui, None)
+dvz.app_onkeyboard(app, on_keyboard, None)
 
 dvz.scene_run(scene, app, 0)
 dvz.scene_destroy(scene)
