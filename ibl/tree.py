ff1779e6124a25f6a6868141de57413fa51109c1
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-02 21:52:38 +0200

    Add tree example with Allen brain regions as tree view

--- ibl/tree.py
+++ ibl/tree.py
@@ -17,21 +17,18 @@ selected = np.zeros(n - 1, dtype=np.bool)
 visible = np.ones(n - 1, dtype=np.bool)
 haystack = np.array([f"{a} {n}".lower() for a, n in zip(acronyms, names)])
 folded[levels >= 5] = True
-
-
-app = dvz.app(0)
-batch = dvz.app_batch(app)
-scene = dvz.scene(batch)
-figure = dvz.figure(scene, 800, 600, dvz.CANVAS_FLAGS_IMGUI)
-
 search = dvz.CStringBuffer("")
 
 
-@dvz.on_gui
-def on_gui(app, fid, ev):
+app = dvz.App()
+figure = app.figure(gui=True)
+
+
+@app.connect(figure)
+def on_gui(ev):
     dvz.gui_pos(vec2(25, 25), vec2(0, 0))
     dvz.gui_size(vec2(500, 500))
-    dvz.gui_begin("My GUI", 0)
+    dvz.gui_begin("Allen brain regions", 0)
 
     if dvz.gui_textbox("Search", 64, search, 0):
         s = search.value.lower()
@@ -44,15 +41,5 @@ def on_gui(app, fid, ev):
     dvz.gui_end()
 
 
-# panel1 = dvz.panel(figure, 50, 50, 300, 300)
-# dvz.demo_panel3D(panel1)
-# dvz.panel_gui(panel1, "Panel 1", 0)
-
-# panel2 = dvz.panel(figure, 400, 100, 300, 300)
-# dvz.demo_panel2D(panel2)
-# dvz.panel_gui(panel2, "Panel 2", 0)
-
-dvz.app_gui(app, dvz.figure_id(figure), on_gui, None)
-dvz.scene_run(scene, app, 0)
-dvz.scene_destroy(scene)
-dvz.app_destroy(app)
+app.run()
+app.destroy()
