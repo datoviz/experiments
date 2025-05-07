c69348c25b171d7c6e9ab725c5b558726dea2f6c
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-07 21:03:18 +0200

    Update michael.py example

--- ibl/michael.py
+++ ibl/michael.py
@@ -1,59 +1,48 @@
 import math
 import numpy as np
 import datoviz as dvz
-from datoviz import Out
 
 
-def make_texture(batch, image):
+def make_texture(app, image):
     assert image.ndim == 2
-    format = dvz.FORMAT_R8_UNORM
-    image *= .3
+    image *= 0.3
     normalized = dvz.to_byte(image, 0, 1)
-    return dvz.texture_image(batch, format, dvz.FILTER_NEAREST, dvz.SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER, image.shape[1], image.shape[0], normalized, 0)
+    return app.texture_2D(normalized)
 
 
-def add_image(x, y, w, h, image, batch=None, panel=None):
+def add_image(x, y, w, h, image, app=None, panel=None):
     pos = np.array([[x, y, 0]], dtype=np.float32)
     size = np.array([[w, h]], dtype=np.float32)
     anchor = np.array([[-1, 0]], dtype=np.float32)
     texcoords = np.array([[0, 0, 1, 1]], dtype=np.float32)
-    texture = make_texture(batch, image)
+    texture = make_texture(app, image)
 
-    visual = dvz.image(
-        batch, dvz.IMAGE_FLAGS_SIZE_NDC | dvz.IMAGE_FLAGS_RESCALE | dvz.IMAGE_FLAGS_MODE_COLORMAP)
-    dvz.image_alloc(visual, 1)
-    dvz.image_position(visual, 0, 1, pos, 0)
-    dvz.image_size(visual, 0, 1, size, 0)
-    dvz.image_colormap(visual, dvz.CMAP_BINARY)
-    dvz.image_anchor(visual, 0, 1, anchor, 0)
-    dvz.image_texcoords(visual, 0, 1, texcoords, 0)
-    dvz.image_texture(visual, texture)
-    dvz.panel_visual(panel, visual, 0)
-
-
-def get_extent():
-    xmin = Out(0.0, 'double')
-    xmax = Out(0.0, 'double')
-    ymin = Out(0.0, 'double')
-    ymax = Out(0.0, 'double')
-    dvz.panzoom_bounds(panzoom, ref, xmin, xmax, ymin, ymax)
+    visual = app.image(
+        rescale=True,
+        position=pos,
+        size=size,
+        anchor=anchor,
+        texcoords=texcoords,
+        unit="ndc",
+        colormap="binary",
+        texture=texture,
+    )
+    panel.add(visual)
 
 
-path = 'concat_ephysFalse.npy'
+path = "concat_ephysFalse.npy"
 r = np.load(path, allow_pickle=True).flat[0]
-data = r['concat_z'][r['isort']]
+data = r["concat_z"][r["isort"]]
 print(f"Loaded data with shape {data.shape}")
 
-cols_beryl = np.load('cols_beryl.npy')
-cols_beryl = cols_beryl[r['isort']]
+# cols_beryl = np.load("cols_beryl.npy")
+# cols_beryl = cols_beryl[r["isort"]]
 
-FIG_SIZE = 800
-app = dvz.app(dvz.APP_FLAGS_WHITE_BACKGROUND)
-batch = dvz.app_batch(app)
-scene = dvz.scene(batch)
-figure = dvz.figure(scene, FIG_SIZE, FIG_SIZE, 0)
-panel = dvz.panel_default(figure)
-panzoom = dvz.panel_panzoom(panel, 0)
+
+app = dvz.App(background="white")
+figure = app.figure()
+panel = figure.panel()
+panzoom = panel.panzoom()
 
 x = -1
 w = 2
@@ -62,19 +51,8 @@ row = 4096  # maximum texture size
 h = 2 * row / float(data.shape[0])
 n = math.floor(data.shape[0] / float(row))
 for i in range(n):
-    add_image(x, y - i * h, w, h,
-              data[i*row:(i+1)*row, :], batch=batch, panel=panel)
+    add_image(x, y - i * h, w, h, data[i * row : (i + 1) * row, :], app=app, panel=panel)
 
-ref = dvz.ref(0)
-dvz.ref_set(ref, dvz.DIM_X, 0, 1)
-dvz.ref_set(ref, dvz.DIM_Y, 0, 1)
 
-# @dvz.on_frame
-# def on_frame(app, window_id, ev):
-#     get_extent()
-# dvz.app_on_frame(app, on_frame, None)
-
-dvz.scene_run(scene, app, 0)
-dvz.scene_destroy(scene)
-dvz.app_destroy(app)
-dvz.ref_destroy(ref)
+app.run()
+app.destroy()
