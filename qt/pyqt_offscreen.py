09d7837c6a7e83a0f79a97db4419cf36689617f4
Cyrille Rossant <rossant@users.noreply.github.com>
2025-07-09 16:04:03 +0200

    Add pyqt_offscreen.py proof of concept

---
+++ qt/pyqt_offscreen.py
@@ -0,0 +1,56 @@
+"""# PyQt6 local example
+
+Show how to integrate offscreen Datoviz figures into a PyQt6 application, using the Datoviz
+server API which provides a fully offscreen renderer with support for multiple canvases.
+
+NOTE: this API is experimental and will change in an upcoming release.
+
+"""
+
+import sys
+
+try:
+    from PyQt6.QtCore import Qt
+    from PyQt6.QtWidgets import QApplication, QMainWindow, QSplitter
+except:
+    from PyQt5.QtCore import Qt
+    from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter
+
+import datoviz as dvz
+from datoviz.backends.pyqt6 import QtServer
+
+WIDTH, HEIGHT = 800, 600
+
+
+class ExampleWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Example Qt Datoviz window")
+
+        # Create a Qt Datoviz server.
+        self.qt_server = QtServer()
+
+        # Create two figures (special Qt widgets with a Datoviz figure).
+        w, h = WIDTH // 2, HEIGHT
+        self.qt_figure1 = self.qt_server.create_figure(w, h)
+        self.qt_figure2 = self.qt_server.create_figure(w, h)
+
+        # Fill the figures with mock data.
+        dvz.demo_panel_2D(dvz.panel(self.qt_figure1.figure, 0, 0, w, h))
+        dvz.demo_panel_2D(dvz.panel(self.qt_figure2.figure, 0, 0, w, h))
+
+        # Add the two figures in the main window.
+        splitter = QSplitter(Qt.Orientation.Horizontal)
+        splitter.addWidget(self.qt_figure1)
+        splitter.addWidget(self.qt_figure2)
+        splitter.setCollapsible(0, False)
+        splitter.setCollapsible(1, False)
+        self.setCentralWidget(splitter)
+        self.resize(WIDTH, HEIGHT)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mw = ExampleWindow()
+    mw.show()
+    sys.exit(app.exec())
